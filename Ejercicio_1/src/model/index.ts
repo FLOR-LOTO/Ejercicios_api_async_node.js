4e8ebcc2c3a18d2a0758f0d0da4e8aceb2c90dc7
Flor_Loto <florloto.ge@gmail.com>
2024-07-14 00:37:08 -0300

    Ejercicio 1: Done!

--- Ejercicio_1/src/model/index.ts
+++ Ejercicio_1/src/model/index.ts
@@ -4,7 +4,6 @@ const fetchData = async (city: string, date: string) => {
   const response = await fetch(
     `${URL_API}/forecast.json${KEY}${city}&dt=${date}`
   );
-
   const data = await response.json();
   const { maxtemp_c, mintemp_c } = data.forecast.forecastday[0].day;
   return { maxtemp_c, mintemp_c };
